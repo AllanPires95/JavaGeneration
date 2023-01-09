a220af41b48b41b5aa1267d0d79e47229ab34476
AllanPires95 <120184333+AllanPires95@users.noreply.github.com>
2023-01-09 11:25:14 -0300

    version 1.1

---
+++ Curso.java
@@ -0,0 +1,102 @@
+//package POO;
+//public class Curso {
+//	private String Nome;
+//	private String Endereco;
+//	private String Modalidade;
+	//private String Duracao;
+	//private double Renda;
+	
+	
+	//public String getNome() {
+		//return Nome;
+	//}
+	//public void setNome(String nome) {
+		//this.Nome = nome;
+	//}
+//	public String getEndereco() {
+	//	return Endereco;
+	//}
+	//public void setEndereco(String endereco) {
+		//this.Endereco = endereco;
+	//}
+	//public String getModalidade() {
+		//return Modalidade;
+	//}
+	//public void setModalidade(String modalidade) {
+		//this.Modalidade = modalidade;
+//	}
+	//public double getRenda() {
+		//return Renda;
+	//}
+	//public void setRenda(double renda) {
+		//this.Renda = renda;
+	//}
+	//public String getDuracao() {
+		//return Duracao;
+//	}
+	//public void setDuracao(String duracao) {
+		//this.Duracao = duracao;
+//	}
+	
+	
+//}
+//*package POO;
+//*public class CursoLivre extends Curso {
+	
+//*	private String TI;
+	
+//*	public CursoLivre(String Nome, String Endereco) {
+//*	}
+
+//*	public String getTI() {
+//*		return TI;
+//*	}
+
+//*	public void setTI(String tI) {
+//*		this.TI= tI;
+//*	}
+	
+//*}
+//*package POO;
+
+//public class Especializacao extends Curso {
+//	private String CienciaDeDados;
+	
+//	public Especializacao (String Nome, String Endereco, String Modalidade, double Salario,String Duracao, String Periodo, String Escola) {
+		
+//	}
+	
+//	public Especializacao(String Generation, String eAD) {
+		// TODO Auto-generated constructor stub
+//	}
+
+	//public String getCienciaDeDados() {
+		//return CienciaDeDados;
+	//}
+
+//	public void setCienciaDeDados(String cienciaDeDados) {
+	//	this.CienciaDeDados = cienciaDeDados;
+	//}
+//}
+
+	//*package POO;
+
+//*public class TestaCurso {
+
+//*	public static void main(String[] args, String Manha, String EAD, Object Semestral) {
+//*		Curso Programacao = new Curso();
+//*		CursoLivre Direito = new CursoLivre ("TI",Manha);
+//*		Especializacao CienciasDeDados = new Especializacao("Generation",EAD);
+		
+//*		System.out.println(Programacao.getNome());
+//*		System.out.println(Direito.getModalidade());
+//*		System.out.println(Programacao.getDuracao());
+//*		System.out.println(CienciasDeDados.getEndereco());
+		
+		
+		
+		
+//*}
+//*}
+
+
